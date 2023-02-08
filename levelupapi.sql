48c982bdb8922b8ffdf8320a1ad50212abb1f4e0
Vanessa Spear <vanessavspear@gmail.com>
2023-02-08 09:04:16 -0600

    Client can now post a new event and a new game

--- levelupapi.sql
+++ levelupapi.sql
@@ -3,3 +3,6 @@ SELECT * FROM levelupapi_gametype;
 SELECT * FROM auth_user;
 SELECT * FROM authtoken_token;
 SELECT * FROM levelupapi_gamer;
+
+DELETE FROM levelupapi_game
+WHERE id > 3;
--- levelupapi/views/game_view.py
+++ levelupapi/views/game_view.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from levelupapi.models import Game
+from levelupapi.models import Game, Gamer, GameType
 
 
 class GameView(ViewSet):
@@ -30,9 +30,30 @@ class GameView(ViewSet):
         serializer = GameSerializer(game, many=True)
         return Response(serializer.data)
 
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+            Response -- JSON serialized game instance
+        """
+        gamer = Gamer.objects.get(user=request.auth.user)
+        type = GameType.objects.get(pk=request.data["game_type"])
+
+        game = Game.objects.create(
+            title=request.data["title"],
+            maker=request.data["maker"],
+            maximum_players=request.data["maximum_players"],
+            minimum_players=request.data["minimum_players"],
+            skill_level=request.data["skill_level"],
+            gamer=gamer,
+            type= type
+        )
+        serializer = GameSerializer(game)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 class GameSerializer(serializers.ModelSerializer):
     """JSON serializer for games
     """
     class Meta:
         model = Game
-        fields = ('id', 'title', 'num_players', 'maker', 'skill_level', 'gamer_id', 'type')
+        fields = ('id', 'title', 'minimum_players', 'maximum_players', 'maker', 'skill_level', 'gamer_id', 'type')
